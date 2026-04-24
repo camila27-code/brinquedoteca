f8aea6ab00a5cb7c38fc9ef5df0c0737d3951598
Camila Lima <camila_lima62@outlook.com>
2026-04-23 23:44:17 -0300

    Update criancas.py

--- schemas/criancas.py
+++ schemas/criancas.py
@@ -1,15 +1,16 @@
 from pydantic import BaseModel
+from datetime import date
+from enum import Enum
 
+class CriancaCreate(BaseModel):
+    nome: str
+    idade: int
+    responsavel: str
 
-class CriancasCreate(BaseModel):
-     id: int
-     nome: str
-     idade: int
-     responsavel: str
-    
-
-class CriancasOut(BaseModel):
+class CriancaOut(BaseModel):
     id: int
     nome: str
     idade: int
     responsavel: str
+    bloqueado: bool
+    atrasos: int
