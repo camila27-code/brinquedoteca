dbc960845171d0a266881b407e5b97f76a5f2256
Camila Lima <camila_lima62@outlook.com>
2026-04-23 21:01:39 -0300

    Add Pydantic models for Emprestimos

--- schemas/emprestimos.py
+++ schemas/emprestimos.py
@@ -1,1 +1,19 @@
+from pydantic import BaseModel
 
+
+class EmprestimosCreate(BaseModel):
+     id: int
+     crianca_id: int
+     brinquedo_id: int
+     datas: str
+     status: str
+     multa: int
+    
+
+class EmprestimosOut(BaseModel):
+     id: int
+     crianca_id: int
+     brinquedo_id: int
+     datas: str
+     status: str
+     multa: int
